38e30b0281d687a7b07f6e0734b354bbc9c7c6d9
davidecarizzoni <davide.carizzoni@runelab.it>
2024-01-08 20:58:27 +0100

    faet(headlessUI): install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-headlessui'
+  ],
   css: ['@fortawesome/fontawesome-svg-core/styles.css'],
   app: {
     head: {
       title: 'X Clone'
     }
+  },
+  headlessui: {
+    prefix: 'Headless'
   }
 })
