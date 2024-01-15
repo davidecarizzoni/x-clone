8ef2aff59619737b9b1b90fa25cf1532fa86d1a1
davidecarizzoni <davide.carizzoni@runelab.it>
2024-01-15 21:48:01 +0100

    feat(prisma)

--- server/db/users.js
+++ server/db/users.js
@@ -1,7 +1,11 @@
 import { prisma } from "~/server/db/index.js";
+import bcrypt from 'bcrypt';
 
 export const createUser = (userData) => {
   return prisma.user.create({
-    data: userData,
+    data: {
+      ...userData,
+      password: bcrypt.hashSync(userData.password, 10)
+    },
   })
 }
